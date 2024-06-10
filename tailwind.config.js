296d1a574a59d6b511cbd6cabadf58f4bdb6d3db
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-06-10 11:56:22 +0530

    tailwind css lecture 3 added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./main/**/*.html"],
+  theme: {
+    fontFamily: {
+
+    'ansh': ['Helvetica', 'Arial', 'sans-serif'],
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
+}
